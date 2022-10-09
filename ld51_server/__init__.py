cf73455159cee3435d69358dd70b2993861d9aee
Simon Berger <simon@siku2.io>
2022-10-09 22:57:32 +0200

    add CORS middleware

--- ld51_server/__init__.py
+++ ld51_server/__init__.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import dev, game
 
 app = FastAPI(
     title="LD51 Server",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(game.router)
